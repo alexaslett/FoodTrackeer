93e94d3a11d0ce96d0ff30ef20abd4eaead93e1f
Alex Aslett <alexaslett@slkc.firstdigital.com>
2017-07-16 19:07:37 -0600

    Trying to fix something

---
+++ FoodTrackeer/MealTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  MealTableViewCell.swift
+//  FoodTrackeer
+//
+//  Created by Alex Aslett on 7/16/17.
+//  Copyright © 2017 One Round Technology. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    //MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
